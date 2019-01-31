27de6cca7a8c0263d15283e52f3acba716ff7ed4
Jesus Soto <sotous0@gmail.com>
2019-01-31 15:14:38 -0400

    Another Fix c:

--- server/controllers/system.js
+++ server/controllers/system.js
@@ -8,7 +8,7 @@ function change_hour(req, res, next){
     let date = req.body.date;
     let hour = req.body.hour;
     console.log('date -s "'+date+' '+hour+'"');
-    //shell.exec('date -s "'+date+' '+hour+'"')
+    shell.exec('date -s "'+date+' '+hour+'"')
     res.sendStatus(200);
     return;
 }
