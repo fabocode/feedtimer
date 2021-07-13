e35985623252d990e4ecc66cdffac415793cce2f
Fabian <fabianbarraez@gmail.com>
2021-07-13 15:54:35 -0500

    test: 💍 add files from RaspiWiFi repo

--- server/public/js/input.ctrl.js
+++
@@ -1,9 +0,0 @@
-function clear_onclick(elem){
-    elem.value = "";
-}
-
-function auto_fill(elem){
-    if(elem.value == ""){
-        elem.value =0;
-    }
-}
--- server/router/real_time.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let router = require('express').Router();
-let ClockCtrl = require('../controllers/real_time');
-
-router.get('/', ClockCtrl.get_time);
-
-module.exports = router;
